e5234547f45b2ce5b379f5cb8c2561965ffc695e
Shu Ohmichi <shu.ohm1@gmail.com>
2023-07-10 16:41:56 +0900

    update: a script to update host keys

--- .ssh/known_hosts_diff.bash
+++ .ssh/known_hosts_diff.bash
@@ -1,4 +1,4 @@
 #!/bin/bash
 diffcmd=diff
 [ -x "$(/bin/which colordiff 2> /dev/null)" ] && diffcmd=colordiff
-${diffcmd} -s -u known_hosts <(cat hostkey_*)
+${diffcmd} -u known_hosts <(cat hostkey_*)
